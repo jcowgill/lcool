e0e5391689d4591e6e5470c458b3c52f9c6353c3
James Cowgill <james410@cowgill.org.uk>
2014-05-26 21:27:11 +0100

    Add logger and location classes

---
+++ src/logger.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2014 James Cowgill
+ *
+ * LCool is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LCool is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LCool.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <boost/format.hpp>
+#include <iostream>
+#include <string>
+
+#include "logger.hpp"
+
+std::string lcool::location::to_string() const
+{
+	return boost::str(boost::format("%1%:%2%:%3%") % filename % line % column);
+}
+
+std::ostream& operator<< (std::ostream& stream, const lcool::location& loc)
+{
+	return stream << loc.to_string();
+}
+
+lcool::logger_ostream::logger_ostream()
+	: stream(std::clog)
+{
+}
+
+lcool::logger_ostream::logger_ostream(std::ostream& stream)
+	: stream(stream)
+{
+}
+
+bool lcool::logger_ostream::has_errors() const
+{
+	return errors_printed;
+}
+
+void lcool::logger_ostream::warning(const std::string& str)
+{
+	stream << "warning: " << str << std::endl;
+}
+
+void lcool::logger_ostream::warning(const lcool::location& loc, const std::string& str)
+{
+	stream << loc << ": warning: " << str << std::endl;
+}
+
+void lcool::logger_ostream::error(const std::string& str)
+{
+	stream << "error: " << str << std::endl;
+	errors_printed = true;
+}
+
+void lcool::logger_ostream::error(const lcool::location& loc, const std::string& str)
+{
+	stream << loc << ": error: " << str << std::endl;
+	errors_printed = true;
+}
---
+++ src/logger.hpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2014 James Cowgill
+ *
+ * LCool is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LCool is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LCool.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LCOOL_LOGGER_HPP
+#define LCOOL_LOGGER_HPP
+
+#include <ostream>
+#include <string>
+
+namespace lcool
+{
+	/** A position in a code file (used for logging errors and debugging) */
+	class location
+	{
+	public:
+		/** Filename of this position */
+		std::string filename;
+
+		/** Start line of the position (first line is 1) */
+		uint32_t line;
+
+		/** Start column of the position (first column is 1) */
+		uint32_t column;
+
+		/** Convert this location into a string */
+		std::string to_string() const;
+	};
+
+	/** Prints a location to an output stream (according to to_string) */
+	std::ostream& operator<< (std::ostream& stream, const location& loc);
+
+	/** Class used to log compiler warnings and errors */
+	class logger
+	{
+	public:
+		virtual ~logger() { }
+
+		/** Returns true if errors have occured */
+		virtual bool has_errors() const = 0;
+
+		/** Prints a warning */
+		virtual void warning(const std::string& str) = 0;
+
+		/** Prints a warning which occured at the given location */
+		virtual void warning(const location& loc, const std::string& str) = 0;
+
+		/** Prints an error */
+		virtual void error(const std::string& str) = 0;
+
+		/** Prints an error which occured at the given location */
+		virtual void error(const location& loc, const std::string& str) = 0;
+	};
+
+	/** Implementation of logger which prints to an ostream */
+	class logger_ostream : public logger
+	{
+	public:
+		/** Constructs a logger using clog as its stream */
+		logger_ostream();
+
+		/** Constructs a logger using the given stream */
+		explicit logger_ostream(std::ostream& stream);
+
+		virtual bool has_errors() const;
+		virtual void warning(const std::string& str);
+		virtual void warning(const location& loc, const std::string& str);
+		virtual void error(const std::string& str);
+		virtual void error(const location& loc, const std::string& str);
+
+	private:
+		std::ostream& stream;
+		bool errors_printed = false;
+	};
+}
+
+#endif
