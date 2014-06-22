f376af3abae1c359a96e57cde333ade627fd12a0
James Cowgill <james410@cowgill.org.uk>
2014-06-22 01:30:10 +0100

    Added program_structure classes

---
+++ src/builtins.hpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2014 James Cowgill
+ *
+ * LCool is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LCool is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LCool.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LCOOL_BUILTINS_HPP
+#define LCOOL_BUILTINS_HPP
+
+namespace lcool
+{
+	class program_structure;
+
+	/**
+	 * Adds all the builtin classes to the given program structure
+	 */
+	void add_builtins(program_structure& program);
+}
+
+#endif
